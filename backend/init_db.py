d0253689b0024fbebf31b7d57622909d9bd94bea
olamide44 <olamideyusuf32@gmail.com>
2025-09-01 23:50:15 +0100

    init db code

--- backend/init_db.py
+++ backend/init_db.py
@@ -1,1 +1,7 @@
- 
+# init_db.py
+from database import engine, Base
+import models  # registers models on Base
+
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=engine)
+    print("✅ Tables created")
